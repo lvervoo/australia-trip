ff732f338b0e93b80c9e0c54f656b15230a52c51
lvervoo <vervoort.loic@gmail.com>
2022-11-04 11:10:34 +1100

    maj

--- gallery.js
+++ gallery.js
@@ -29,14 +29,27 @@ async function getjsonimagesgallery() {
     }
 }
 
+function randomize(tab) {
+    var i, j, tmp;
+    for (i = tab.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        tmp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = tmp;
+    }
+    return tab;
+}
+
 async function CreateGalery(){
 
     let photolist = await getjsonimagesgallery(); //l'appel doit se faire dans une fonction asynchrone pour que ça marche
   
     //Fonction map pour récupérer les données json des slides
     let srcliste = photolist.map(data => data.src);
-    let altliste = photolist.map(data => data.alt);
+    let altliste = photolist.map(data => data.alt); //Tous les altliste sont identiques
     
+    srcliste = randomize(srcliste);
+
     //On ajoute toute les slide en dom
     for (let i = 0; i < srcliste.length; i++) {
         addImagegalerie(srcliste[i],altliste[i]);
